2c6ae0c8ee9cbe561ecee6eb894a14b46c6c2567
Márcio <marcioadrianoalvesjunior@gmail.com>
2021-01-14 17:11:34 -0300

    Selecting hand and going to vs page working

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// 0 = Scissors
+// 1 = Paper
+// 2 = Rock
+
 let
     selectedHand = 0,
     wheelRotation = 0;
@@ -7,7 +11,8 @@ const
     menu_shapes = document.querySelectorAll('.hand-wheel .back-shape path'),
     menu_hands = document.querySelectorAll(".hand-wheel .hand path"),
     menu_turns = document.querySelectorAll('.turn'),
-    menu_wheel = document.querySelector('.hand-wheel')
+    menu_wheel = document.querySelector('.hand-wheel'),
+    menu_playBtn = document.querySelector('.play-btn').addEventListener('click', startGame)
 
 // Menu Selection
     function selectHand(idx){
@@ -57,4 +62,73 @@ const
                 selectHand(selectedHand+1)
             }
         })
+// VS area
+function startGame(){
 
+    //DETECT WINNER
+    console.log(`The winner is: ${theWinneIs()}`)
+    //HIDE MENU ELEMENTS
+    document.querySelector(".select-to-play").classList.toggle("hidden")
+
+    //SHOW NEXT SCREEN ELEMENTS
+    document.querySelector('.background').classList.toggle('vs-screen')
+    document.querySelector('.vs-screen-game').classList.toggle('active')
+}
+
+function theWinneIs(){
+    const
+        comp = numToHand(Math.floor(Math.random() * 3)),
+        player = numToHand(selectedHand)
+
+    console.log(`Player: ${player}
+Computer: ${comp}`)
+
+
+    if(player == comp){
+        return "even"
+    }else 
+    if(player == "scissors"){
+        if(
+        comp == "paper"){
+        return "player"
+        }
+        if(
+        comp == "rock"){
+        return "comp"
+        }
+    }else 
+    if(player == "paper"){
+        if(
+        comp == "scissors"){
+        return "comp"
+        }
+        if(
+        comp == "rock"){
+        return "player"
+        }
+    }else{
+        if(
+        comp == "scissors"){
+        return "player"
+        }
+        if(
+        comp == "paper"){
+        return "comp"
+        }
+    }
+
+    
+}
+
+function numToHand(num){
+    switch(num){
+        case 0:
+            return "scissors"
+            break
+        case 1:
+            return "paper"
+            break
+        case 2:
+            return "rock"
+    }
+}
