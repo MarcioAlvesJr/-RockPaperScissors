ba4214c37306b428b4afed274dca4559022b469a
Márcio <marcioadrianoalvesjunior@gmail.com>
2021-01-14 11:02:26 -0300

    Wheel hands ajusted

--- app.js
+++ app.js
@@ -9,51 +9,52 @@ const
     menu_turns = document.querySelectorAll('.turn'),
     menu_wheel = document.querySelector('.hand-wheel')
 
+// Menu Selection
+    function selectHand(idx){
+        if(idx != selectedHand){
+            rotateWheel();
+            menu_shapes.forEach(shape=>{
+                shape.parentElement.classList.remove('active')
+            })
+            menu_hands.forEach(hand=>{
+                hand.parentElement.classList.remove('active')
+            })
+            menu_shapes[idx].parentElement.classList.add('active')
+            menu_hands[idx].parentElement.classList.add('active')
+        
+            const handwheel = document.querySelector('.hand-wheel')
 
-function selectHand(idx){
-    if(idx != selectedHand){
-        rotateWheel();
-        menu_shapes.forEach(shape=>{
-            shape.parentElement.classList.remove('active')
-        })
-        menu_hands.forEach(hand=>{
-            hand.parentElement.classList.remove('active')
-        })
-        menu_shapes[idx].parentElement.classList.add('active')
-        menu_hands[idx].parentElement.classList.add('active')
-    
-        const handwheel = document.querySelector('.hand-wheel')
+            handwheel.classList.remove('paper')
+            handwheel.classList.remove('rock')
 
-        handwheel.classList.remove('paper')
-        handwheel.classList.remove('rock')
-
-        if(idx == 1 ){
-            handwheel.classList.add('paper')
-        }else if(idx == 2 )
-        {
-            handwheel.classList.add('rock')
+            if(idx == 1 ){
+                handwheel.classList.add('paper')
+            }else if(idx == 2 )
+            {
+                handwheel.classList.add('rock')
+            }
+            }
+            selectedHand = idx
         }
-        }
-        selectedHand = idx
+    function rotateWheel(){
+        menu_wheel.style.transform = `rotate(${wheelRotation}deg)`
     }
-function rotateWheel(){
-    menu_wheel.style.transform = `rotate(${wheelRotation}deg)`
-}
 
-    menu_turns[0].addEventListener("click", () => {
-        wheelRotation = wheelRotation +120
-        if(selectedHand == 0){
-            selectHand(2)
-        }else{
-            selectHand(selectedHand-1)
-        }
-    })
-    menu_turns[1].addEventListener("click", () => {
-        wheelRotation = wheelRotation -120
+        menu_turns[1].addEventListener("click", () => {
+            wheelRotation = wheelRotation +120
+            if(selectedHand == 0){
+                selectHand(2)
+            }else{
+                selectHand(selectedHand-1)
+            }
+        })
+        menu_turns[0].addEventListener("click", () => {
+            wheelRotation = wheelRotation -120
 
-        if(selectedHand == 2){
-            selectHand(0)
-        }else{
-            selectHand(selectedHand+1)
-        }
-    })
+            if(selectedHand == 2){
+                selectHand(0)
+            }else{
+                selectHand(selectedHand+1)
+            }
+        })
+
